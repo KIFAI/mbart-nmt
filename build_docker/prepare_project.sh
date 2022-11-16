3ab1b73a99eb9ab0afc78d388cda89ddb30ae838
jhkim43 <jihyo.kim43@gmail.com>
2022-11-16 14:37:35 +0900

    chore : add ommision case of project path

--- build_docker/prepare_project.sh
+++ build_docker/prepare_project.sh
@@ -2,4 +2,5 @@ export REPO=${PWD%/*}
 cp -pr $REPO/FastModel ./project
 sudo rm -rf $PWD/project/FastModel/Onnx-MBart
 cp -pr $REPO/Server ./project
+cp -pr $REPO/Translate ./project
 cp -pr $REPO/WebApp ./project
